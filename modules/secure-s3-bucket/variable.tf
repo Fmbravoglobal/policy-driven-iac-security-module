14e738fa42f24bafdb8aca37b6963bd87f4ed37b
Oluwafemi Alabi Okunlola <ooluwafemiokunlola308@gmail.com>
2026-03-02 20:54:55 -0600

    feat(module): add required input variables for secure S3 bucket module

--- modules/secure-s3-bucket/variable.tf
+++ modules/secure-s3-bucket/variable.tf
@@ -1,4 +1,9 @@
 variable "bucket_name" {
-  description = "Name of the S3 bucket"
+  description = "Name of the main S3 bucket"
+  type        = string
+}
+
+variable "kms_key_arn" {
+  description = "KMS key ARN for S3 bucket encryption"
   type        = string
 }
