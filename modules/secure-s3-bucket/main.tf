343e5b4839dda26d7125a675a180fdc769a4b95f
Oluwafemi Alabi Okunlola <ooluwafemiokunlola308@gmail.com>
2026-03-02 19:32:04 -0600

    feat: enforce KMS encryption and enable bucket keys for S3 security compliance

--- modules/secure-s3-bucket/main.tf
+++ modules/secure-s3-bucket/main.tf
@@ -30,8 +30,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm = "aws:kms"
     }
+    bucket_key_enabled = true
   }
 }
 
